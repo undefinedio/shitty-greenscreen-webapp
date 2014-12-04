e254295dfda8db5cfa4a90022569ea3b56217d41
koray.sels <koray.sels@gmail.com>
2014-12-04 14:57:45 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express')
+    , fs = require('fs')
+    , bodyParser = require('body-parser');
+var port = process.env.PORT || 9000; // set our port
+var app = express();
+const GREENSCREEN_DIR = 'public/images/greenscreens/';
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+function getRandom(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+app.get('/randomImage.jpg', function (req, res) {
+
+
+    fs.readdir(GREENSCREEN_DIR, function (err, files) {
+        var randomIndex = getRandom(0, (files.length - 1));
+        var randomimage = files[randomIndex];
+        console.log('serving ' + GREENSCREEN_DIR + randomimage);
+        fs.readFile(GREENSCREEN_DIR + randomimage, function (err, data) {
+            res.writeHead(200, {'Content-Type': 'image/jpeg'});
+            res.end(data); // Send the file data to the browser.
+        });
+    });
+});
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
