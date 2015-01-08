a68dbc521e7e249a8550b26ca1e05839c61bc22c
benjamindemoor <demoor.benjamin@gmail.com>
2015-01-08 22:58:58 +0100

    shitty share pic added

--- public/js/main.js
+++ public/js/main.js
@@ -158,7 +158,7 @@ require([
             title = encodeURIComponent("Shitty Party Gallery");
             descr = encodeURIComponent("This was the shitty party. The biggest muilfestijn from Antwerpen and surrounding parking. A big up to the Muiltatuli and the lovely crowd. See you next time!");
             url = encodeURIComponent('https://partypics.shittyguide.org/#' + foregroundImage);
-            image = encodeURIComponent('https://partypics.shittyguide.org/shareImage.jpg');
+            image = encodeURIComponent('http://partypics.shittyguide.org/images/share.jpg');
 
             switch (social) {
                 case 'facebook':
