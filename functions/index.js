2e20502db51ef17015a86f8283df3112c2c116d7
Koray S <koray.sels@gmail.com>
2019-12-13 15:36:45 +0100

    add styles and overlay for photobooth
    Signed-off-by: Koray S <koray.sels@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -3,45 +3,45 @@ const functions = require('firebase-functions')
 
 const admin = require('firebase-admin')
 admin.initializeApp({
-    //credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'shitty-radar.appspot.com',
+  //credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'shitty-radar.appspot.com',
 })
 admin.firestore().settings({ timestampsInSnapshots: true })
-const cors = require('cors')({ origin: true })
+const cors = require('cors')()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.getLocation = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
-        const db = admin.firestore()
+  cors(req, res, () => {
+    const db = admin.firestore()
 
-        return db
-            .doc(`/radars/MbtRBMksjOpdICaqr3Z4`)
-            .get()
-            .then(async doc => {
-                res.status(200).json(doc.data())
-            })
-    })
-})
+    return db
+      .doc(`/radars/MbtRBMksjOpdICaqr3Z4`)
+      .get()
+      .then(async doc => {
+        res.status(200).json(doc.data())
+      })
+  })
+});
 
 exports.getImage = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
-        var bucket = admin.storage().bucket()
+  cors(req, res, () => {
+    var bucket = admin.storage().bucket()
 
-        return bucket.getFiles().then(files => {
-            let urls = []
-            const images = files[0]
-            images.forEach(function(entry) {
-                urls.push(
-                    'https://firebasestorage.googleapis.com/v0/b/shitty-radar.appspot.com/o/' +
-                        entry.id +
-                        '?alt=media'
-                )
-            })
-            var rand = urls[Math.floor(Math.random() * urls.length)]
+    return bucket.getFiles().then(files => {
+      let urls = []
+      const images = files[0]
+      images.forEach(function (entry) {
+        urls.push(
+          'https://firebasestorage.googleapis.com/v0/b/shitty-radar.appspot.com/o/' +
+          entry.id +
+          '?alt=media'
+        )
+      })
+      var rand = urls[Math.floor(Math.random() * urls.length)]
 
-            res.status(200).json({url: rand})
-        })
+      res.status(200).json({ url: rand })
     })
+  })
 })
